d7ec9485ad6cd9dd1f9c7a810e433074e3036ef6
patrickJasonDuque <patpatduque26@gmail.com>
2021-01-27 23:01:59 +0800

    added spinner component

---
+++ components/Spinner.tsx
@@ -0,0 +1,13 @@
+import { Spinner, Container } from 'react-bootstrap';
+
+interface Props {}
+
+const Loader: React.FC<Props> = () => {
+  return (
+    <Container className='text-center'>
+      <Spinner animation='grow' variant='light' role='status' style={{ height: '100px', width: '100px' }} />;
+    </Container>
+  );
+};
+
+export default Loader;
