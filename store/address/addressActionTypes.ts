f7e1022c7e1b7676e34532e8bfd8f7be2d24251e
patrickJasonDuque <patpatduque26@gmail.com>
2021-02-15 14:24:03 +0800

    added address reducer

---
+++ store/address/addressActionTypes.ts
@@ -0,0 +1,19 @@
+export const ADD_ADDRESS = 'ADD_ADDRESS';
+export const REMOVE_ADDRESS = 'REMOVE_ADDRESS';
+
+export interface AddressType {
+  address: string;
+  city: string;
+  postalCode: number;
+}
+
+export interface addAddress {
+  type: typeof ADD_ADDRESS;
+  payload: AddressType;
+}
+
+export interface removeAddress {
+  type: typeof REMOVE_ADDRESS;
+}
+
+export type AddressDispatchType = addAddress | removeAddress;
---
+++ store/address/addressReducer.ts
@@ -0,0 +1,42 @@
+import * as actionTypes from './addressActionTypes';
+import Product from '../../models/product';
+
+export interface CartItem {
+  quantity: number;
+  product: Product;
+}
+
+export interface AddressState {
+  address: string;
+  loading?: boolean;
+  city: string;
+  postalCode: number;
+}
+
+const initialState: AddressState = {
+  loading: false,
+  address: null,
+  city: null,
+  postalCode: null
+};
+
+const cartReducer = (state: AddressState = initialState, action: actionTypes.AddressDispatchType): AddressState => {
+  const newState = { ...state };
+
+  switch (action.type) {
+    case actionTypes.ADD_ADDRESS:
+      newState.loading = false;
+      newState.address = action.payload.address;
+      newState.city = action.payload.city;
+      newState.postalCode = action.payload.postalCode;
+      break;
+    case actionTypes.REMOVE_ADDRESS:
+      newState.address = null;
+      newState.city = null;
+      newState.postalCode = null;
+  }
+
+  return newState;
+};
+
+export default cartReducer;
